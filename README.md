b5606022bcb5896e37ab37b6620396bd53f65113
lsivak <larasanders@gmail.com>
2018-05-07 17:50:44 +0000

    adding files to rebuild db

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+* Ruby version - 2.3.3
 
 * System dependencies
 
@@ -22,3 +22,9 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+Application Description
+
+This is a Candy Shop Application. It manages candies, users, and purchases through the use of forms and RESTful routes. Users can purchase candy if they are hungry enough and if they have enough money. Employees can add, edit or delete candies. 
+
+Installation Guide:
+Fork and clone this repo. Then bundle install and rake:db migrate. Connect to rails rails server.
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # resources :purchases
+    
+ root 'users#home'
+    get '/users' => 'users#new'
+ 
+    post '/users' => 'users#create'
+   
+   get '/signin', to: 'sessions#new', as: 'signin'
+   post '/sessions', to: 'sessions#create'
+   get '/signout', to: 'sessions#destroy'
+   delete '/signout', to: 'sessions#destroy'
+   get '/delete', to: 'candies#destroy'
+   delete '/delete', to: 'candies#destroy'
+   # get '/rides', to: 'rides#new'
+  post '/purchases', to: 'purchases#create'
+ 
+   resources :users
+  resources :candies
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
